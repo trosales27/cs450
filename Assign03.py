b14af7dd146c07411392416269bff96b7dd4e9bb
Thomas Rosales <thomasrosales276@gmail.com>
2019-01-28 17:07:51 -0700

    finishing part 2 and 3ish

--- Assign03.py
+++ Assign03.py
@@ -9,18 +9,25 @@ Created on Thu Jan 24 23:21:25 2019
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
 import math
 import pandas as pd
+
+import matplotlib.pyplot as plt
 
 
 #Assign03
 
+"""
+FIRST DATASET
+: File: car.csv
+: Car_data
+"""
 #read in the three data file susing pandas
 #read in car.csv
 names = ["buying","maint","doors","persons","lug_boot","safety", "status"]
 car_data = pd.read_csv('car.csv', header=None, names=names)
-#print("car data is: ")
-#print(car_data)
+
 
 #Giving string data numeric value
 car_data.buying = car_data.buying.astype('category')
@@ -55,37 +62,110 @@ car_data = car_data.drop(columns=['status', 'status_cat'])
 car_data = car_data.values
 #print(car_data)
 
+#.values to convert to numpy Array, ravel to convert to a 1d array
+car_target = car_target.values.ravel()
+
+#split up the data
+data_train, data_test, target_train, target_test = train_test_split(
+        car_data, car_target, test_size=0.30)
+
 classifier = KNeighborsClassifier(n_neighbors=5)
-classifier.fit(car_data, car_target)
-predictions = classifier.predict(car_data)
+classifier.fit(data_train, target_train)
+predictions = classifier.predict(data_test)
 
-car_target = car_target.values
-
-car_target = car_target.ravel()
-results = (predictions == car_target)
+results = (predictions == target_test)
 
 correct = 0
 for i in results:
     if i == True:
         correct += 1
-print("There were " + str(correct) + " correct estimates out of " + str(len(results)) +
-      " for " + str(100 * round(correct / len(results), 2)) + "% accuracy")
+#print("There were " + str(correct) + " correct estimates out of " + str(len(results)) +
+#      " for " + str(100 * round(correct / len(results), 2)) + "% accuracy\n")
 
+
+"""
+SECOND DATASET
+: File: auto-mpg.data
+: mpg_data
+"""
 #read in auto-mpg.data
 mpg_data = pd.read_csv('auto-mpg.data', header=None, delim_whitespace=True, na_values=["?"])
 mpg_data.columns = ["mpg","cylinders","displacement","horsepower","weight",
                  "acceleration","modelyear","origin","carname"]
-mpg_data.horsepower = mpg_data.horsepower.fillna("unknown")
-#print("mpg_data is:")
+
+#replace unknown values with mean of column
+mean = mpg_data['horsepower'].mean()
+mpg_data.horsepower = mpg_data.horsepower.fillna(mean)
+
+mpg_target = mpg_data[['mpg']]
+mpg_data = mpg_data[['cylinders','displacement','horsepower','weight',
+                 'acceleration','modelyear','origin']]
+
+#Plot to a graph to see visual representation
+#mpg_data['horsepower'].plot(kind='hist', bins=100)
+#plt.xlabel('MPG Value')
+
+mpg_target = mpg_target.values.ravel()
+mpg_data = mpg_data.values
+
+#split up the data
+data_train, data_test, target_train, target_test = train_test_split(
+        mpg_data, mpg_target, test_size=0.30)
 
 
+regr = KNeighborsRegressor(n_neighbors=5)
+regr.fit(data_train, target_train)
+predictions = regr.predict(data_test)
 
 
+results = (predictions == target_test)
+
+correct = 0
+for i in results:
+    if i == True:
+        correct += 1
+#print("There were " + str(correct) + " correct estimates out of " + str(len(results)) +
+#      " for " + str(100 * round(correct / len(results), 2)) + "% accuracy\n")
+
+"""
+THIRD DATASET
+: File: student-mat.csv
+: student_data
+"""
 #read in student-mat.csv
-student_data = pd.read_csv('student-mat.csv', sep=";", header=None)
-#print("Student_data is: ")
-#print(student_data)
+student_data = pd.read_csv('student-mat.csv', sep=";")
+
+print("Student_data is: ")
+print(student_data)
+
+#Replacing two-option values
+cleanup = {"school": {"GP":0, "MS":1}, "sex": {"M":0, "F":1},
+           "address": {"U":0, "R":1}, "famsize": {"GT3":0, "LE3":1},
+           "guardian": {"father":0, "mother":1}, "schoolsup": {"yes":0, "no":1},
+           "famsup": {"yes":0, "no":1}, "paid": {"yes":0, "no":1}, 
+           "activities": {"yes":0, "no":1},"nursery": {"yes":0, "no":1},
+           "higher": {"yes":0, "no":1}, "internet": {"yes":0, "no":1}, 
+           "romantic": {"yes":0, "no":1} 
+           }
+student_data.replace(cleanup, inplace=True)
+print("Student_data is: ")
+print(student_data)
+
+#Mjob and Fjob have 5 options: teacher, health, services, at_home, and other
+#reason has 4 options: course, home, reuptation, and other
+#replace these 4 appropriately, then predict like normal
+
+"""regr = KNeighborsRegressor(n_neighbors=5)
+regr.fit(data_train, target_train)
+predictions = regr.predict(data_test)
 
 
+results = (predictions == target_test)
 
+correct = 0
+for i in results:
+    if i == True:
+        correct += 1
+#print("There were " + str(correct) + " correct estimates out of " + str(len(results)) +
+#      " for " + str(100 * round(correct / len(results), 2)) + "% accuracy\n")"""
 
