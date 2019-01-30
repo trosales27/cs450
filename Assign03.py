0acc580e9a7df2096b8c6f263098cab3fd309db6
Thomas Rosales <thomasrosales276@gmail.com>
2019-01-29 20:22:48 -0700

    fixed empty data

--- Assign03.py
+++ Assign03.py
@@ -141,31 +141,46 @@ print(student_data)
 #Replacing two-option values
 cleanup = {"school": {"GP":0, "MS":1}, "sex": {"M":0, "F":1},
            "address": {"U":0, "R":1}, "famsize": {"GT3":0, "LE3":1},
-           "guardian": {"father":0, "mother":1}, "schoolsup": {"yes":0, "no":1},
+           "Pstatus": {"A":0, "T":1}, "schoolsup": {"yes":0, "no":1},
            "famsup": {"yes":0, "no":1}, "paid": {"yes":0, "no":1}, 
-           "activities": {"yes":0, "no":1},"nursery": {"yes":0, "no":1},
-           "higher": {"yes":0, "no":1}, "internet": {"yes":0, "no":1}, 
+           "activities": {"yes":0, "no":1}, "nursery": {"yes":0, "no":1}, 
+           "higher": {"yes":0, "no":1}, "internet": {"yes":0, "no":1},
            "romantic": {"yes":0, "no":1} 
            }
 student_data.replace(cleanup, inplace=True)
 print("Student_data is: ")
 print(student_data)
 
-#Mjob and Fjob have 5 options: teacher, health, services, at_home, and other
-#reason has 4 options: course, home, reuptation, and other
-#replace these 4 appropriately, then predict like normal
 
-"""regr = KNeighborsRegressor(n_neighbors=5)
+#use one-hot encoding for the remaining values
+student_data = pd.get_dummies(student_data, columns=['Mjob', 'Fjob', 
+                                                     'reason', 'guardian'])
+print("Check 2")
+print(student_data)
+
+student_target = student_data[['G3']]
+student_data = student_data.drop(columns=['G3'])
+
+#onvert to numpy array
+student_target = student_target.values.ravel()
+student_data = student_data.values
+
+#print(student_target)
+#print(student_data)
+
+#split up the data
+data_train, data_test, target_train, target_test = train_test_split(
+        student_data, student_target, test_size=0.30)
+
+regr = KNeighborsRegressor(n_neighbors=5)
 regr.fit(data_train, target_train)
 predictions = regr.predict(data_test)
 
-
-results = (predictions == target_test)
 
 correct = 0
 for i in results:
     if i == True:
         correct += 1
-#print("There were " + str(correct) + " correct estimates out of " + str(len(results)) +
-#      " for " + str(100 * round(correct / len(results), 2)) + "% accuracy\n")"""
+print("There were " + str(correct) + " correct estimates out of " + str(len(results)) +
+      " for " + str(100 * round(correct / len(results), 2)) + "% accuracy\n")
 
