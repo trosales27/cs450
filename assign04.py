a9b091bab54694e57f03bf7d0556641a1351b57b
Thomas Rosales <thomasrosales276@gmail.com>
2019-02-06 18:01:33 -0700

    entropy equation

---
+++ assign04.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 31 21:38:59 2019
+
+@author: trosales
+"""
+
+import math
+from collections import Counter
+
+class decision_tree:
+    
+    #Needs to calculate entropy for each category
+    #create branches based of the highest entropy
+    
+    def calcEntropy(self, data_set):
+
+        #set data_set to an array
+        data = []
+        for i in data_set:
+            data.append(i)
+               
+        print(Counter(data))
+        #Make a list of the categories
+        categories = Counter(data_set)
+
+        labels = []
+        for i in Counter(data):
+            labels.append(i)
+            
+        h = 0    
+        
+        #This for loop begins the summation to calculate entropy for the dataset
+        for i in labels:
+            #print(i, categories[i])
+            h -= ((categories[i]/len(data)) * math.log2(categories[i]))
+            
+        h *= -1
+        print("h is:")
+        print(h)
+        
+        
+        
+#testing area        
+test = decision_tree()
+data = ['dog', 'cat', 'dog', 'dog', 'cat', 'cat']
+test.calcEntropy(data)
